bb17f81de269f4fc58092ae0967cd936b18fd0e6
pjh <bluetruddnjs1@gmail.com>
2019-01-04 17:44:12 +0900

    D03 | 190104 PM | Recursive function, swexpert

--- study/swexpert/level2/1948.py
+++ study/swexpert/level2/1948.py
@@ -1,7 +1,20 @@
 n = int(input())
 
-month = [31,28,31,30,31,30,31,31,30,31,30,31]
+month = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
 for tc in range (1,n+1):
+    day = 0
     temp = list(map(int,input().split()))
+    # 같은 월
+    if temp[0] == temp[2]:
+        day = temp[3] - temp[1] +1
+    # 1달 차이
+    elif temp[2]-temp[0] == 1:
+        day += temp[3] + month[temp[0]]-temp[1] +1
+    # 그외
+    else:
+        for i in range (temp[0]+1,temp[2]):
+            day += month[i]
+        day += temp[3] + month[temp[0]]-temp[1] +1
     
+    print(f'#{tc} {day}')
---
+++ study/swexpert/level2/1954.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for tc in range(1,n+1):
+    num = int(input())
+    llist = [[0]*num for in range(num)]
+    i = 0
+    j = 0
+    for n in range (1,num**2+1):
+        
+        
