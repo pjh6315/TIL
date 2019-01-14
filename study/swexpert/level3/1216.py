11ca8c272597b2bd173d2e470651287c3d1cf175
pjh <bluetruddnjs1@gmail.com>
2019-01-14 17:52:07 +0900

    D09 | 190114 PM | HTML & SWEXPERT

--- study/swexpert/level3/1216.py
+++ study/swexpert/level3/1216.py
@@ -4,27 +4,14 @@ def palin(s):
     else:
         return False
 
-for tc in range(1,11):
+for tc in range(1,2):
     n = int(input())
+    ans = 0
     llist = []
-    cnt = 0
+    cnt = []
     for i in range(100):
         llist.append(input())
-    
-    for i in range(0,100):
-        for j in range(0,101-n):
-            temp = ''
-            for k in range(0,n):
-                temp+=llist[i][j+k]
-            if palin(temp):
-                cnt+=1
+    print(type(llist[0]))
 
-    for i in range(0,101-n):
-        for j in range(0,100):
-            temp = ''
-            for k in range(0,n):
-                temp+=llist[i+k][j]
-            if palin(temp):
-                cnt+=1
     
-    print(f'#{tc} {cnt}')
+    print(f'#{tc} {ans}')
