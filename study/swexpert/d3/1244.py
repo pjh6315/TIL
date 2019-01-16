63342d3da0f6a9b2306b661a2e77c8179fe4c0c2
pjh <bluetruddnjs1@gmail.com>
2019-01-16 12:22:31 +0900

    D11 | 190116 AM | bootstrap & swexpert

--- study/swexpert/d3/1244.py
+++ study/swexpert/d3/1244.py
@@ -1,12 +1,36 @@
 t = int(input())
 
-
-
-
 for tc in range(1,t+1):
-    money, n = map(int,input().split())
+    money, n = map(str,input().split())
+    cha_m= []
+    if len(money) == 1 :
+        ans  = money
+    else:
+        for i in range(int(n)):
+            length = len(cha_m)
+            if i == 0:
+                for j in range(0,len(money)-1):
+                    for k in range(j+1,len(money)):
+                        temp = []
+                        temp = list(money)
+                        temp[j],temp[k] = temp[k], temp[j]
+                        cha_m.append(temp)
+                        kkk = set(cha_m)
+                        cha_m = list(kkk)
+            else:
+                for o in range(length):
+                    a=cha_m.pop(0)
+                    for j in range(0,len(a)-1):
+                        for k in range(j+1,len(a)):
+                            temp = []
+                            temp = list(a)
+                            temp[j],temp[k] = temp[k], temp[j]
+                            cha_m.append(temp)
+                            kkk = set(cha_m)
+                            cha_m = list(kkk)
+            
 
-    for i in range(n):
-        for 
-
-
+    final = []
+    for d in range(0,len(cha_m)):
+        final.append(int(''.join(cha_m[d])))
+    print(f'#{tc} {max(final)}')
