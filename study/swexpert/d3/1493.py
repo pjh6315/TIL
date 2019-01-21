a6502f498a18f784a7e5b11de7494cd30d1a1606
pjh <bluetruddnjs1@gmail.com>
2019-01-21 17:56:59 +0900

    SWEXPERT

--- study/swexpert/d3/1493.py
+++ study/swexpert/d3/1493.py
@@ -1,1 +1,26 @@
-dasdasdsdsadassadas
+t = int(input())
+
+def wow(x):
+    n=1
+    while (n**2-n+2)/2 <= x :
+        n+=1
+    i = int(x - (((n-1)**2-(n-1)+2)/2))  
+
+    return 1+i,n-1-i
+
+def rev_wow(x,y):
+    n= x+y-1
+    i = (n**2-n+2)/2
+
+    return int(i+x-1)
+
+    
+    
+for tc in range(1,t+1):
+    p,q = map(int,input().split())
+    
+    p = wow(p)
+    q = wow(q)
+
+    print(f'#{tc} {rev_wow(p[0]+q[0],p[1]+q[1])}')
+
---
+++ study/swexpert/d3/1860.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for tc in range(1,t+1):
+    n,m,k = map(int,input().split())
+    man = list(map(int,input().split()))
+
+    
