a219f15bc6414037f1d6146a8029da451c0133bc
pjh <bluetruddnjs1@gmail.com>
2019-01-24 17:50:09 +0900

    swexpert

--- study/swexpert/d3/2814.py
+++ study/swexpert/d3/2814.py
@@ -1,9 +1,45 @@
+from collections import defaultdict
+
 t = int(input())
+
+def dfs(g,v,visited=None,path=None):
+    if visited is None:
+        visited = []
+    if path is None:
+        path = [v]
+    
+    visited.append(v)
+    paths=[]
+    for point in g[v]:
+        if point not in visited:
+            temp_path = path + [point]
+            paths.append(tuple(temp_path))
+            paths.extend(dfs(g,point,visited[:],temp_path))
+    return paths
 
 for tc in range(1,t+1):
     n,m = map(int,input().split())
     point = []
-    for i in range(m):
-        point.append(list(map(int,input().split())))
+    all_paths = []
+    max_len = 0
+    graph = {}
+    if m == 0:
+        max_len = 1
+    else:
+        for i in range(m):
+            point.append(list(map(int,input().split())))
+        
+        graph = defaultdict(list)
+        for s,t in point:
+            graph[s].append(t)
+            graph[t].append(s)
+
+        for point in graph.keys():
+            all_paths.extend(dfs(graph,point))
+        
+        max_len = max(len(path) for path in all_paths )
+        
+    print(f'#{tc} {max_len}')
     
-    print(point)
+
+
---
+++ study/swexpert/d3/2817.py
@@ -0,0 +1,36 @@
+t = int(input())
+
+def dfs(llist, start,want,visited=None,path=None):
+    if visited is None:
+        visited = []
+    if path is None:
+        path = [llist[start]]
+    
+    visited.append(start)
+    print(visited)
+    paths=[]
+    for i in range(start+1,len(llist)):
+        if i not in visited:
+            if sum(path) + llist[i] == want:
+                temp = path + [llist[i]]
+                paths.append(tuple(temp))
+            elif sum(path) + llist[i] < want:
+                temp = path + [llist[i]]
+                paths.extend(dfs(llist,i,want,visited,temp))
+    return paths
+
+
+
+for tc in range(1,t+1):
+    n,k = map(int,input().split())
+    a = list(map(int,input().split()))
+    all_paths = []
+
+
+    for i in range(n):
+        if a[i] == k :
+            all_paths.append(a[i])
+        else:
+            all_paths.extend(dfs(a,i,k))
+    print(all_paths)
+    print(f'#{tc} {len(all_paths)}')
