91a148d65c10026c130e0557ea8816e2d2d65c9d
pjh <bluetruddnjs1@gmail.com>
2019-01-28 17:50:12 +0900

    swexpert

--- study/swexpert/d3/2817.py
+++ study/swexpert/d3/2817.py
@@ -1,29 +1,45 @@
 t = int(input())
 
-def dfs(llist, start,want,visited=None,path=None):
-    if visited is None:
-        visited = []
-    if path is None:
-        path = [llist[start]]
+# def dfs(llist, start,want,visited=None,path=None):
+#     if visited is None:
+#         visited = []
+#     if path is None:
+#         path = [llist[start]]
     
-    visited.append(start)
-    print(visited)
-    paths=[]
-    for i in range(start+1,len(llist)):
-        if i not in visited:
-            print(llist[i])
-            if sum(path) + llist[i] == want:
-                temp = path + [llist[i]]
-                # paths.extend(dfs(llist,i,want,visited,temp))
-                paths.append(tuple(temp))
-                # temp = None
-                # dfs(llist,start,want,visited,temp)
-            elif sum(path) + llist[i] < want:
-                temp = path + [llist[i]]
-                paths.extend(dfs(llist,i,want,visited,temp))
-            else:
-                return paths
-    return paths
+#     visited.append(start)
+#     print(visited)
+#     paths=[]
+
+
+
+    # for i in range(start+1,len(llist)):
+    #     if i not in visited:
+    #         print(llist[i])
+    #         if sum(path) + llist[i] == want:
+    #             temp = path + [llist[i]]
+                
+    #             paths.append(tuple(temp))
+                
+    #         elif sum(path) + llist[i] < want:
+    #             temp = path + [llist[i]]
+    #             paths.extend(dfs(llist,i,want,visited,temp))
+    #         else:
+    #             return paths
+    # return paths
+
+def dfs(llist,start,want,result=None,temp=None):
+    if result == None:
+        result = []
+    if temp == None:
+        temp = []
+    temp.append(llist[start])
+    if start == len(llist) or sum(temp) > want:
+        if sum(temp) == want:
+            result.append(temp)
+    else:
+        dfs(llist,start+1,want,result,temp)
+    return result
+
 
 
 
