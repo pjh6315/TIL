364360ae9274fb591754bc22a94868437b54f398
pjh <bluetruddnjs1@gmail.com>
2019-01-29 17:56:48 +0900

    swexpert

--- study/swexpert/d3/2817.py
+++ study/swexpert/d3/2817.py
@@ -32,27 +32,35 @@ def dfs(llist,start,want,result=None,temp=None):
         result = []
     if temp == None:
         temp = []
-    temp.append(llist[start])
-    if start == len(llist) or sum(temp) > want:
-        if sum(temp) == want:
-            result.append(temp)
-    else:
-        dfs(llist,start+1,want,result,temp)
+
+    for i in range(start,len(llist)):
+        t = sum(temp) + llist[i]
+        tttt = temp + [llist[i]]
+        if t < want :      
+                                                                                                                   
+            result.extend(dfs(llist,start+1,want,result,tttt))
+        elif t == want :
+            result.append(tuple(tttt))
+
     return result
+        
+        
+    
 
 
 
-
+                                    
 for tc in range(1,t+1):
     n,k = map(int,input().split())
     a = list(map(int,input().split()))
     all_paths = []
 
 
-    for i in range(n):
-        if a[i] == k :
-            all_paths.append(a[i])
-        else:
-            all_paths.extend(dfs(a,i,k))
+    # for i in range(n):
+    #     if a[i] == k :
+    #         all_paths.append(a[i])
+    #     else:
+    #         all_paths.extend(dfs(a,i,k))
+    all_paths= dfs(a,0,k)
     print(all_paths)
     print(f'#{tc} {len(all_paths)}')
